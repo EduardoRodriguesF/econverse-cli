857dab4abd015a61330e76113c9f9f0c615eb915
gabriel sampaio <gabriel.sampaio@econverse.com.br>
2022-05-30 16:58:22 -0300

    fix some weird dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import clear from 'clear';
-import chalk from 'chalk';
-import figlet from 'figlet';
+import { Command } from 'commander';
+// import clear from 'clear';
+// import chalk from 'chalk';
+// import figlet from 'figlet';
 
-program.description('Econverse CLI');
-program.name('econverse');
-program.usage('econverse <command> [options]');
+const program = new Command();
 
-clear();
-console.log(
-  chalk.green(figlet.textSync('Econverse CLI', { horizontalLayout: 'full' })),
-);
+program
+  .name('econverse')
+  .description('Econverse CLI')
+  .usage('econverse <command> [options]')
+  .version('0.0.1');
+
+// TERMINAL: node -r ts-node/register src/index.ts help split
+program
+  .command('split')
+  .description('Split a string into substrings and display as an array')
+  .argument('<string>', 'string to split')
+  .option('--first', 'display just the first substring')
+  .option('-s, --separator <char>', 'separator character', ',')
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
+
+program.parse();
+// clear();
+// console.log(
+//   chalk.green(figlet.textSync('Econverse CLI', { horizontalLayout: 'full' })),
+// );
