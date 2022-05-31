67158602c96e282933228e32da0ad0b74a7b3288
Gabriel Sampaio <gabriel.sampaio@econverse.com.br>
2022-05-31 09:08:43 -0300

    refactoring & testing :tada:

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
 #!/usr/bin/env ts-node
 
 import { program } from 'commander';
+import chalk from 'chalk';
+import clear from 'clear';
+import axios from 'axios';
 
-program.description('Econverse CLI');
-program.name('econverse');
-program.usage('<command> [options]');
-
-console.log('Econverse CLI');
+interface TodoParams {
+  id: string;
+}
 
 program
-  .command('init')
-  .argument('[accountName]', "VTEX store's account name")
-  .description('Initialize a new project');
+  .name('econverse')
+  .description('Econverse CLI')
+  .usage('econverse <command> [options]')
+  .version('0.0.1');
 
-program.parse();
+async function getTodos({ id }: TodoParams) {
+  const URL = `https://jsonplaceholder.typicode.com/todos/${id}`;
+  const { data } = await axios.get(URL);
+
+  console.log(data);
+}
+
+// yarn ts-node src/index.ts todos --id 2
+program
+  .command('todos')
+  .description("retrieve a specific todo based on it's id")
+  .option('--id <id>', "Todo's id to be displayed", '1')
+  .action(getTodos);
+
+program.parse(process.argv);
+
+// clear();
+// console.log(chalk.yellowBright('Econverse'));
